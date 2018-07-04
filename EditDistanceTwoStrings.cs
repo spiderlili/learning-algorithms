30b0e475cec13c55c5637eaa8d5d4a7a92c203b1
Jing Chun Tan <jtan1993@gmail.com>
2018-07-04 13:18:23 +0100

    Update EditDistanceTwoStrings.cs

--- EditDistanceTwoStrings.cs
+++ EditDistanceTwoStrings.cs
@@ -10,9 +10,7 @@ static bool isEditDistanceOne(String str1,String str2){
     // Find lengths of given strings
     int str1Length = str1.Length, str2Length = str2.Length;
  
-    // If difference between lengths is 
-    // more than 1, then strings can't 
-    // be at one distance
+    // If difference between lengths > 1, then strings can't be at one distance
     if (Math.Abs(str1Length - str2Length) > 1)
         return false;
  
@@ -20,27 +18,21 @@ static bool isEditDistanceOne(String str1,String str2){
         int count = 0;
         int i = 0, j = 0;
          
-    while (i < str1Length && j < str2Length)
-    {
-        // If current characters 
-        // don't match
+    while (i < str1Length && j < str2Length){
+        // If current characters don't match
         if (str1[i] != str2[j])
         {
             if (count == 1)
                 return false;
  
-            // If length of one string is
-            // more, then only possible edit
-            // is to remove a character
+            // If length of one string is more, then only possible edit is to remove a character
             if (str1Length > str2Length)
                 i++;
             else if (str1Length < str2Length)
                 j++;
                  
-             // If lengths of both 
-             // strings is same
-            else
-            {
+             // If lengths of both strings is the same
+            else{
                 i++;
                 j++;
             }
@@ -50,15 +42,13 @@ static bool isEditDistanceOne(String str1,String str2){
         }
  
         // If current characters match
-        else
-        {
+        else{
             i++;
             j++;
         }
     }
  
-    // If last character is extra 
-    // in any string
+    // If last character is extra in any string
     if (i < str1Length || j < str2Length)
         count++;
  
